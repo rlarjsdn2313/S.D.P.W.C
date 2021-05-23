5cd27c23ee762c0f53001c80ef8199c2ee026608
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-23 15:31:59 +0900

    Change chrome to firefox

--- main.py
+++ main.py
@@ -2,28 +2,13 @@ import os
 import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 def ChangeIP():
     os.system('/etc/init.d/tor restart')
     time.sleep(1)
 
-chrome_options = Options()
-chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
-driver = webdriver.Chrome(executable_path=f"{os.getcwd()}/chromedriver", options=chrome_options)
+driver = webdriver.Firefox(executable_path=f'{os.getcwd()}/geckodriver')
 
 driver.get('http://icanhazip.com/')
-
-ChangeIP()
+print(driver.page_source)
 driver.quit()
-
-chrome_options = Options()
-chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
-driver = webdriver.Chrome(executable_path=f"{os.getcwd()}/chromedriver", options=chrome_options)
-
-driver.get('http://icanhazip.com/')
-time.sleep(3)
-driver.quit()
-
-ChangeIP()
-
